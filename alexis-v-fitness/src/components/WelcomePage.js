caf3d6f246366ef8f819eca559e4c46b7d050b95
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-05-14 20:11:59 -0500

    Changed the title on Welcome Page.

--- alexis-v-fitness/src/components/WelcomePage.js
+++ alexis-v-fitness/src/components/WelcomePage.js
@@ -16,7 +16,7 @@ const WelcomePage = () => {
                 <Card marginTop='60px'>
                     <Card.Img variant="top" src="holder.js/100px160" />
                     <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>Welcome!</Card.Title>
                         <Card.Text>
                             This is a wider card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.
