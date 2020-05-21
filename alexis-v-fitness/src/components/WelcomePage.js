c63bcd9051e5b55eb76fcf8323a49f8136bbbf08
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-05-21 16:55:10 -0500

    Added more...
    I added the workout snippets in the Welcome Page and added my Youtube workout clips that can be opened in another tab.

--- alexis-v-fitness/src/components/WelcomePage.js
+++ alexis-v-fitness/src/components/WelcomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WelcomeHeader, Icons } from './StyledWidgets';
+import { WelcomeHeader, Icons, IMG } from './StyledWidgets';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import { SocialIcon } from 'react-social-icons';
@@ -15,48 +15,50 @@ const WelcomePage = () => {
 
             <CardDeck margin-top='60px'>
                 <Card marginTop='60px'>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    
                     <Card.Body>
                         <Card.Title>Welcome!!</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Body>
-                        <Card.Link href="/workout">Card Link</Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <IMG>
+                            <img className="img-container" src={require('../imgs/Squats.png')} alt="img of Alexis." />
+                        </IMG>
                         <Card.Text>
                             This card has supporting text below as a natural lead-in to additional
                             content.{' '}
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
+                    <Card.Body>
+                        <Card.Link href="/workout">Workout</Card.Link>
+                        <Card.Link href="https://youtu.be/kKT2RVW6RJU" target="_blank">Workout Demo</Card.Link>
+                    </Card.Body>
+                    
                 </Card>
+
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
                     <Card.Body>
                         <Card.Title>Card title</Card.Title>
+                        <IMG>
+                            <img className="img-container" src={require('../imgs/Lunge Twists.png')} alt="img of Alexis." />
+                        </IMG>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
+                            content.{' '}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <IMG>
+                            <img className="img-container" src={require('../imgs/Criss-Cross Crunches.png')} alt="img of Alexis." />
+
+                            
+                        </IMG>
                         <Card.Text>
                             This is a wider card with supporting text below as a natural lead-in to
                             additional content. This card has even longer content than the first to
                             show that equal height action.
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
             </CardDeck>
 
